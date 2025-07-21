ed3109d745350926e5e7e24f923d32606120ad64
Pawan gupta <pawangupta@Pawans-MacBook-Air.local>
2025-07-22 01:21:27 +0530

    Debug: api1

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
-console.log('🚨 Supabase Anon Key:', supabaseKey);
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    headers: {
+      apikey: supabaseAnonKey,
+     
+    },
+  },
+});
